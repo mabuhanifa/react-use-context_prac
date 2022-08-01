1ba7de8e84d8cf92edab3d55be3ac160a3848f40
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-02 01:41:08 +0600

    cart using use state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import useShop from "./context/api/ShopContext";
+import { useState } from "react";
 import { data } from "./data";
 
 function App() {
-  const { total, products, addToCart, removeFromCart } = useShop();
-  console.log(total, products);
+  // const { total, products, addToCart, removeFromCart } = useShop();
+  // console.log(total, products);
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
+  console.log(cart);
   return (
     <div style={{ margin: "100px" }}>
+      <h2>Total Item Selected :{cart.length}</h2>
+      <h2>
+        Total Price:
+        {cart.reduce((t, cart) => {
+          return t + cart.price;
+        }, 0)}
+      </h2>
       {data.map((product) => {
         return (
           <div style={{ border: "1px solid gray" }}>
