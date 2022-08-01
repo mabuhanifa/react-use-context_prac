0205a0c1f5123675182fe6f31ad532e4a09aae5e
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-02 01:03:06 +0600

    context updated

--- src/context/api/ShopContext.js
+++ src/context/api/ShopContext.js
@@ -1,3 +1,51 @@
-import { createContext } from "react";
+import { createContext, useContext, useReducer } from "react";
+import { initialState, shopReducer } from "../reducer/shopReducer";
 
-const ShopContext = createContext();
+const ShopContext = createContext(initialState);
+
+export const ShopProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(shopReducer, initialState);
+
+  const addToCart = (product) => {
+    const updatedCart = state.products.concat(product);
+    updatePrice(updatedCart);
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: { products: updatedCart },
+    });
+  };
+  const removeFromCart = (product) => {
+    const updatedCart = state.products.filter((p) => p.id !== product.id);
+    updatePrice(updatedCart);
+    dispatch({
+      type: "REMOVE_FROM_CART",
+      payload: { products: updatedCart },
+    });
+  };
+  const updatePrice = (products) => {
+    let total = 0;
+    products.forEach((product) => (total += product.price));
+    dispatch({
+      type: "UPDATE_PRICE",
+      payload: { total },
+    });
+  };
+  const value = {
+    total: state.total,
+    products: state.products,
+    addToCart,
+    removeFromCart,
+  };
+
+  return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>;
+};
+
+const useShop = () => {
+  const context = useContext(ShopContext);
+  if (context === undefined) {
+    throw new Error("ShopContext is undefined");
+  }
+  return context;
+};
+
+export default useShop;
