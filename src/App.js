4b67465d25adda511c0bf4639e25c19e3b936c17
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-02 01:28:30 +0600

    cart updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import useShop from "./context/api/ShopContext";
+import { data } from "./data";
+
 function App() {
+  const { total, products, addToCart, removeFromCart } = useShop();
+  console.log(total, products);
   return (
-    <div>
-      
+    <div style={{ margin: "100px" }}>
+      {data.map((product) => {
+        return (
+          <div style={{ border: "1px solid gray" }}>
+            <h1>{product.name}</h1>
+            <button onClick={() => addToCart(product)}>Add</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
