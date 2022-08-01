d8335f7df85c3fb866fb66a4bfc37fefa6dafa49
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-01 23:57:14 +0600

    data added

---
+++ data.js
@@ -0,0 +1,56 @@
+export let data = [{
+    id: '1',
+    name: "AirPods Max",
+    price: 549,
+    img: "https://www.apple.com/v/airpods-max/d/images/overview/hero__gnfk5g59t0qe_large.png"
+},
+{
+    id: "2",
+    name: "Micropack MHP",
+    price: 100,
+    img: "https://www.startech.com.bd/image/cache/catalog/headphone/micropack/mhp-01/mhp-01-01-500x500.jpg"
+},
+{
+    id: "3",
+    name: "Havit HV-139D",
+    price: 90,
+    img: "https://www.startech.com.bd/image/cache/catalog/headphone/a4tech/hs-9/a4tech-hs-9-stereo-headphone-black-500x500.jpg"
+},
+{
+    id: "4",
+    name: "Sony WH-CH710N",
+    price: 205,
+    img: "https://www.startech.com.bd/image/cache/catalog/headphone/sony/wh-ch710n/wh-ch710n-500x500.jpg"
+},
+{
+    id: "5",
+    name: "Astrum HS110",
+    price: 80,
+    img: "https://www.startech.com.bd/image/cache/catalog/headset/astrum/hs110/hs110-01-500x500.jpg"
+},
+{
+    id: "6",
+    name: "Havit HV-H100d",
+    price: 59,
+    img: "https://www.startech.com.bd/image/cache/catalog/headphone/havit/h100d/h100d-1-500x500.jpg"
+},
+{
+    id: "7",
+    name: "Sony WH-CH500",
+    price: 75,
+    img: "https://www.startech.com.bd/image/cache/catalog/headphone/sony/sony-wh-ch500/sony-wh-ch500-500x500.jpg"
+},
+{
+    id: "8",
+    name: "Havit 2218D",
+    price: 35,
+    img: "https://www.startech.com.bd/image/cache/catalog/headphone/havit/h2218d/h2218d-500x500.jpg"
+},
+{
+    id: "9",
+    name: "HAVIT HV-H2198d",
+    price: 220,
+    img: "https://www.startech.com.bd/image/cache/catalog/headphone/havit/h2198d/hv-h2198d-black-500x500.jpg"
+}
+
+];
