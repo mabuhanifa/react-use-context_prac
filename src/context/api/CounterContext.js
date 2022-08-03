b46e8554e9e6ae03fe9606dfe25aaa25320580ee
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-03 22:29:04 +0600

    counterContext updated

--- src/context/api/CounterContext.js
+++ src/context/api/CounterContext.js
@@ -1,15 +1,48 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
 
-const count= [52,50];
+const initialState = { count: 0 };
+const countReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case "increment":
+      return (state.count += 1);
 
+    case "decrement":
+      return (state.count += 1);
 
-export const CounterContext = createContext(count);
+    case "incrementBy":
+      return (state.count += payload);
+    default:
+      return state.count;
+  }
+};
+export const CounterContext = createContext(initialState);
 
 export const CounterProvider = (props) => {
-  const [state, setState] = useState(count);
+  const [state, dispatch] = useReducer(countReducer, initialState);
+
+  const increment = () => {
+    dispatch({
+      type: "increment",
+    });
+  };
+  const decrement = () => {
+    dispatch({
+      type: "increment",
+    });
+  };
+  const incrementBy = (payload) => {
+    dispatch({
+      type: "increment",
+      payload: payload,
+    });
+  };
+
   const store = {
-    state,
-    setState,
+    count: state.count,
+    increment,
+    decrement,
+    incrementBy,
   };
   return (
     <CounterContext.Provider value={store}>
